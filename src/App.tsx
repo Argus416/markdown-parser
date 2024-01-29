38bcfb966f23cdb655a67b4ed4dcea5a32c27585
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-01-29 10:22:15 +0100

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import { Button } from 'flowbite-react';
+import { Button } from 'flowbite-react'
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+  const toto = (ee: string) => {
+    console.log('toto', ee)
+  }
 
-	return (
-		<div>
-			<Button>Click me</Button>
-		</div>
-	);
+  useEffect(() => {
+    if (count === 0) {
+      console.log('count is 0')
+    }
+  }, [])
+  return (
+    <div>
+      <img
+        src={reactLogo}
+        width='200'
+        alt='React Logo'
+      />
+      <Button
+        className='prettier-class'
+        id='prettier-id'>
+        Click me
+      </Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
