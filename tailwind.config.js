26039daea7fae25b6e81dbe724f4185848d3e504
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-01-29 09:54:16 +0100

    add flowbit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+export default {
+	content: ['./src/**/*.{js,jsx,ts,tsx}', 'node_modules/flowbite-react/lib/esm/**/*.js'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('flowbite/plugin')],
+};
