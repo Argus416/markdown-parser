3777adf085caff7af2d17de4d45f291bc1ee2e8b
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-01-30 21:41:45 +0100

    Remove comments

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,28 +1,3 @@
-// export function textBetweenPrefix(
-//   regex: RegExp,
-//   prefix: string,
-//   text: string,
-//   baliseHtml: string,
-//   classHtml: string = '',
-//   uniqueReplace = false
-// ): string {
-//   const match = text.match(regex)
-//   if (!match) return text
-
-//   match.forEach((m: string) => {
-//     let formatted = m.replace(prefix, '').trim()
-//     if (!uniqueReplace) {
-//       formatted = m.replaceAll(prefix, '').trim()
-//     }
-//     text = text.replaceAll(
-//       m,
-//       `<${baliseHtml} class="${classHtml}">${formatted}</${baliseHtml}>`
-//     )
-//   })
-
-//   return text
-// }
-
 export function textBetweenPrefix({
   regex,
   prefix,
